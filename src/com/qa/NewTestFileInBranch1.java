39bca95a3357689cae0b907d8bb80c233a6544bb
Lisa <60852071+Savitha-Waheguru@users.noreply.github.com>
2020-02-13 13:26:54 +0530

    my first

---
+++ src/com/qa/NewTestFileInBranch1.java
@@ -0,0 +1,11 @@
+package com.qa;
+
+public class NewTestFileInBranch1 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Test In Branch1 Checkin");
+
+	}
+
+}
---
+++ src/com/qa/NewTestFileInMaster.java
@@ -0,0 +1,10 @@
+package com.qa;
+
+public class NewTestFileInMaster {
+
+	public static void main(String[] args) {
+		System.out.println("Test In Master Checkin");
+
+	}
+
+}
