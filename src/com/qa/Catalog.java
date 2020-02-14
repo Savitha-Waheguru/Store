b5d1f01cf46871026737369bbf14bbe3f49bf71d
Lisa <60852071+Savitha-Waheguru@users.noreply.github.com>
2020-02-14 11:43:05 +0530

    Changes to Catalog and Orders files

--- src/com/qa/Catalog.java
+++ src/com/qa/Catalog.java
@@ -4,13 +4,16 @@ public class Catalog {
 	
 	public static void main(String[] args) 
 	{
-					System.out.println("Catalog");
 					
+		
+					
+		System.out.println("Catalog");
 			//.....................................
 					
-					System.out.println("added this line after commit - Catalog");
+		
+		System.out.println("added this line after commit - Catalog");
 				
-					System.out.println("DIFF CHECK - added this line after commit - Catalog");
+		System.out.println("DIFF CHECK - added this line after commit - Catalog");
 
 	}
 	
---
+++ src/com/qa/Orders.java
@@ -0,0 +1,16 @@
+package com.qa;
+
+public class Orders {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Test In orders ");
+		
+		System.out.println("orders");
+		//.....................................
+				
+	
+	System.out.println("added this line after commit - orders");
+	}
+
+}
